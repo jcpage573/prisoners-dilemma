e4f3de648d117b582daa58b03a741a67129ae77d
hunter <hunterjsb@gmail.com>
2024-06-23 16:11:09 -0400

    auth mw boilerplate

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -11,15 +11,32 @@ type Logger struct {
 	handler http.Handler
 }
 
-// ServeHTTP handles the request by passing it to the real
-// handler and logging the request details
 func (l *Logger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
 	l.handler.ServeHTTP(w, r)
 	log.Printf("%s %s %v", r.Method, r.URL.Path, time.Since(start))
 }
 
-// NewLogger constructs a new Logger middleware handler
 func NewLogger(handlerToWrap http.Handler) *Logger {
 	return &Logger{handlerToWrap}
+}
+
+// Auth is a middleware handler that verifies user requestor identities
+const keyHeaderName string = "X-PRISONER-KEY"
+
+type Auth struct {
+	handler http.Handler
+}
+
+func (a *Auth) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	apiKey := r.Header.Get(keyHeaderName)
+	if apiKey == "" {
+		http.Error(w, "API Key required", http.StatusUnauthorized)
+	}
+
+	a.handler.ServeHTTP(w, r)
+}
+
+func NewAuth(handlerToWrap http.Handler) *Auth {
+	return &Auth{handlerToWrap}
 }
