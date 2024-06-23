0c49d084a10b6ec9a4ec37bb86c202f035e849a1
hunter <hunterjsb@gmail.com>
2024-06-23 13:12:37 -0400

    server boilerplate and /test

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
-	
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("Erd Tree!")) })
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
